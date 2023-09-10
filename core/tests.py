cab37fbaa95d8fb03e2283a6f61a58979b86e3a6
Ramon Brito <ramon.mecatec@gmail.com>
2023-09-10 16:31:16 -0300

    Add tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class PostPageTest(TestCase):
+    def test_uses_post_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, "index.html")
